bbe73d1e9cea5ef217b39624094be680f16600b9
ilman01 <ilmanzidni2006@gmail.com>
2024-05-08 16:49:08 +0700

    Update pnpm version

--- InstallVencord.py
+++ InstallVencord.py
@@ -1,7 +1,7 @@
 import os
 
 os.system("git pull origin main")
-os.system("npm i -g pnpm@8.10.2")
+os.system("npm i -g pnpm@9.1.0")
 os.system("pnpm i")
 os.system("pnpm build")
 os.system("pnpm inject")
